9c9d9ad9a1a1baee36f25305e2e379e370cbe6aa
danikvh <danivicen@gmail.com>
2022-07-30 14:02:22 +0200

    Function to add a book to the library

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 let myLibrary = [];
 
 function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.info = function() {
-      let readString;
-      if (read === true) readString = "read";
-      else readString = "not read yet";
-      return title + ", " + author + ", " + pages + ", " + readString;
-    }
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.info = function() {
+    let readString;
+    if (read === true) readString = "read";
+    else readString = "not read yet";
+    return title + ", " + author + ", " + pages + ", " + readString;
+  }
 }
 
+function addBookToLibrary() {
+  const title = prompt("What is the book title?") 
+  const author = prompt("Who is the author?") 
+  const pages = prompt("How many pages does the nook have?") 
+  const read = prompt("Have you read the book?") 
+  myLibrary.push(new Book(title, author, pages, read))
+}
