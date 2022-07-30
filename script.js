8b9ad29dffef7d0a61a81d7d54bca2ecb24fe764
danikvh <danivicen@gmail.com>
2022-07-30 15:43:29 +0200

    Functionality to add books with modal window

--- script.js
+++ script.js
@@ -1,38 +1,61 @@
 let myLibrary = [];
 
 let tbodyRef = document.getElementById("myTable").getElementsByTagName("tbody")[0]
+let popupButton = document.getElementById("popup-button")
+let modal = document.getElementById("myModal")
+let submitButton = document.getElementById("submit-button")
+
+popupButton.addEventListener("click", showForm)
+window.addEventListener("click", this.closeModal)
+submitButton.addEventListener("click", addBookToLibrary)
+
 
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
-  this.read = read;
+  this.read === true ? (this.read = "Read") : (this.read = "Not read yet")
   this.info = function() {
-    let readString;
-    if (read === true) readString = "read";
-    else readString = "not read yet";
-    return title + ", " + author + ", " + pages + ", " + readString;
+    return title + ", " + author + ", " + pages + ", " + read;
   }
 }
 
 function addBookToLibrary() {
-  const title = prompt("What is the book title?") 
-  const author = prompt("Who is the author?") 
-  const pages = prompt("How many pages does the nook have?") 
-  const read = prompt("Have you read the book?") 
-  myLibrary.push(new Book(title, author, pages, read))
+  const title = document.querySelector("input[name$='title']").value 
+  const author = document.querySelector("input[name$='author']").value
+  const pages = document.querySelector("input[name$='pages']").value
+  let read = document.querySelector("input[name$='read']").checked
+  let book = new Book(title, author, pages, read)
+  myLibrary.push(book)
+  displayBook(book)
+  modal.style.display = "none";
 }
 
 function displayBooks() {
   myLibrary.forEach((book) => {
-    let newRow = tbodyRef.insertRow()
-    for (let i = 0; i < 4; i++) {
-      let newCell = newRow.insertCell()
-      let newText = document.createTextNode(Object.values(book)[i])
-      newCell.appendChild(newText)
-    }
+    displayBook(book)
   })
 }
 
-myLibrary.push(new Book("fds","fasdgf", "543", "true"))
+function displayBook(book) {
+  let newRow = tbodyRef.insertRow()
+  for (let i = 0; i < 4; i++) {
+    let newCell = newRow.insertCell()
+    let newText = document.createTextNode(Object.values(book)[i])
+    newCell.appendChild(newText)
+  }
+}
+
+function showForm() {
+  modal.style.display = "block"
+}
+
+function closeModal(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
+myLibrary.push(new Book("The Lord of the Rings","J. R. R. Tolkien", "543", "true"))
 displayBooks()
